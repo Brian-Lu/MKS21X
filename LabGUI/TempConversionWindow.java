beb331a0bfa70bf18d8dd859c0a6fc6b71a6fa1a
Brian-Lu <brianlu22@yahoo.com>
2015-12-16 23:10:17 -0500

    Finished up the HW

--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -1,52 +1,59 @@
+import java.util.*;
 import javax.swing.*;
-import java.awt.*;//NEW STUFF!
+import java.awt.*;
 import java.awt.event.*;
 
-public class Window3 extends JFrame implements ActionListener{
+public class TempConversionWindow extends JFrame implements ActionListener{
     private Container pane;
     private JLabel j;
     private JTextField t;
+    private JButton c, f;
 
-    public Window3() {
-	this.setTitle("My first GUI");
+    public TempConversionWindow() {
+	this.setTitle("Temperature Converter");
 	this.setSize(600,400);
 	this.setLocation(100,100);
-	this.setDefaultCloseOperation(
-				      EXIT_ON_CLOSE);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
 	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
-	JButton b = new JButton("ByteMe");
-	b.addActionListener(this);
-	b.setActionCommand("Byte");
-	JButton b2 = new JButton("No...");
-	b2.addActionListener(this);
-	b2.setActionCommand("NotByte");
-
-
+	c = new JButton("Celcius to Fahrenheit");
+	c.addActionListener(this);
+	c.setActionCommand("Celcius-Fahrenheit");
+	f = new JButton("Fahrenheit to Celcius");
+	f.addActionListener(this);
+	f.setActionCommand("Fahrenheit-Celcius");
 	t = new JTextField(10);
-
-	JCheckBox c = new JCheckBox("OverClock");
-	j = new JLabel("Meaning of life is... ");
+	j = new JLabel("Temperature is  ", null, JLabel.CENTER);
 	pane.add(c);
 	pane.add(t);
-	pane.add(b);
-	pane.add(b2);
+	pane.add(f);
 	pane.add(j);
     }
-
+    public static double CtoF (double temp) {
+	return (temp * 1.8) + 32;
+    }
+    public static double FtoC (double temp) {
+	return (temp - 32) / 1.8;
+    }
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
-	if(event.equals("Byte")){
-	    String s = t.getText();
-	    s += "-0101000";
-	    j.setText(s);
+	double init = Double.parseDouble(t.getText());
+	String setter;
+	double converted;
+	if(event.equals("Celcius-Fahrenheit")){
+	    converted = CtoF(init);
+	    setter=String.valueOf(converted);
+	    j.setText(setter);
 	}
-	if(event.equals("NotByte")){
-	    j.setText("Fish");
+	if(event.equals("Fahrenheit-Celcius")){
+	    converted = FtoC(init);
+	    setter=String.valueOf(converted);
+	    j.setText(setter);
+	}
   
-	}
     }
 }
+
 
