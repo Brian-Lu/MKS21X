66a8c65fc17d164417741b74f57726821bb1390f
Brian-Lu <brianlu22@yahoo.com>
2016-01-04 12:11:33 -0500

    Made directory and insertion sort

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,26 @@
+public class Sorts{
+    public static void printArray(int[] data) {
+	System.out.print("[");
+	for (int i = 0; i < data.length - 1; i++) {
+	    System.out.print(data[i] + ", ");
+	}
+	System.out.print(data[data.length - 1]);
+	System.out.println("]");
+    }
+    public static void insertionSort(int[] data) {
+	int x = 0;
+	for (int i = 1; i < data.length; i++) {
+	    if(data[i] < data[i - 1]) {
+		int z = data[i];
+		while (x < data[i]) {
+		    x++;
+		}
+		int y = i;
+		for (y = i; y > x; y--) {
+		    data[y] = data[y-1];
+		}
+		data[x] = z;
+	    }
+	}
+    }
+}
